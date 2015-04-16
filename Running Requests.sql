4b62cbe00a12dd933aba84e3765bf977765c6cc2
kinzleb <brandt.kinzle@radian.biz>
2015-04-16 17:32:41 -0600

    add login_name to requests

--- Running Requests.sql
+++ Running Requests.sql
@@ -1,12 +1,14 @@
 select
 	db_name(a.database_id) as db,
-	user_name(a.user_id) as username,
+	d.login_name,
     substring(b.[text], (statement_start_offset/2) + 1, ((case statement_end_offset when -1 then datalength(b.[text]) else statement_end_offset end - statement_start_offset)/2) + 1) as query_text,
     c.query_plan,
     a.*
 from sys.dm_exec_requests a
 outer apply sys.dm_exec_sql_text(a.[sql_handle]) b
 outer apply sys.dm_exec_query_plan(a.plan_handle) c
+inner join sys.dm_exec_sessions d
+	on d.session_id = a.session_id
 where a.session_id <> @@spid
 	and a.session_id > 50
 	and a.[status] <> 'background' 
