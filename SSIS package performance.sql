81e72d855f3b703ff26d2c102991d0f12463bfba
kinzleb <brandt.kinzle@radian.biz>
2015-04-22 19:01:52 -0600

    added more value to ssis query

--- SSIS package performance.sql
+++ SSIS package performance.sql
@@ -4,26 +4,13 @@ go
 set transaction isolation level read uncommitted
 
 --executions rolled up
-select
-	created_date,
-	count(0) as executions,
-	avg(duration_minutes) as avg_duration_minutes,
-	case min([status])
-		when 1 then 'created'
-		when 2 then 'running'
-		when 3 then 'canceled'
-		when 4 then 'failed'
-		when 5 then 'pending'
-		when 6 then 'ended unexpectedly'
-		when 7 then 'succeeded'
-		when 8 then 'stopping'
-		when 9 then 'completed'
-	end as result
-from (
+;with cte as
+(
 	select
 		datediff(mi, b.start_time, b.end_time) as duration_minutes,
 		convert(date, b.[created_time]) as created_date,
-		b.[status]
+		b.[created_time],
+		c.statusText
 	from (
 		select distinct
 			execution_id
@@ -34,14 +21,50 @@ from (
 	) a
 	inner join [internal].[execution_info] b
 		on b.execution_id = a.execution_id
+	left outer join (
+		values
+			(1, 'created'),
+			(2, 'running'),
+			(3, 'canceled'),
+			(4, 'failed'),
+			(5, 'pending'),
+			(6, 'ended unexpectedly'),
+			(7, 'succeeded'),
+			(8, 'stopping'),
+			(9, 'completed')
+	) c([status], statusText)
+		on c.[status] = b.[status]
+)
+select
+	a.created_date,
+	a.executions,
+	a.avg_duration_minutes,
+	stuff(b.statusList.value('.', 'varchar(8000)'), 1, 2, '') as statusList
+from (
+	select
+		a.created_date,
+		count(0) as executions,
+		avg(a.duration_minutes) as avg_duration_minutes
+	from cte a
+	group by
+		a.created_date
 ) a
-group by
-	created_date
+cross apply (
+	select
+		[text()] = ', ' + a1.statusText + ' (' + convert(varchar, row_number() over (order by a1.[created_time])) + ' in ' + isnull(convert(varchar, a1.duration_minutes),'NA') + ' min)'
+	from cte a1
+	where a1.created_date = a.created_date
+	order by a1.[created_time]
+	for xml path(''), type
+) b(statusList)
+order by
+	created_date desc
 
 
---execution summary
+--execution summary showing process (data) date
 select
 	datediff(mi, b.start_time, b.end_time) as duration_minutes,
+	c.ProcessDate,
 	b.[created_time],
 	b.[execution_id],
 	b.[folder_name],
@@ -57,7 +80,7 @@ select
 		when 7 then 'succeeded'
 		when 8 then 'stopping'
 		when 9 then 'completed'
-	end as result,
+	end as statusText,
 	b.[stopped_by_name],
 	b.end_time
 from (
@@ -70,8 +93,16 @@ from (
 ) a
 inner join [internal].[execution_info] b
 	on b.execution_id = a.execution_id
+cross apply (
+	select top 1
+		convert(datetime, parameter_value) as ProcessDate
+	from [catalog].[execution_parameter_values] c1
+	where c1.parameter_data_type = 'DateTime'
+		and c1.parameter_name = 'ProcessDate'
+		and c1.execution_id = a.execution_id
+) c
 order by
-	duration_minutes desc
+	b.[created_time] desc
 
 
 --execution message detail
@@ -130,6 +161,6 @@ select
 	end as execution_result,
 	execution_path
 from [catalog].[executable_statistics]
-where execution_id = 356624                              --@parmaterize
+where execution_id = 404649                              --@parmaterize
 order by
 	execution_duration_minutes desc
