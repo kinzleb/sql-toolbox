602129e1f5e34096bb6d9711eefe27654e8fe1b0
kinzleb <brandt.kinzle@radian.biz>
2015-05-07 07:00:14 -0600

    added elapsed_minutes to running requests

--- Running Requests.sql
+++ Running Requests.sql
@@ -1,6 +1,7 @@
 select
 	db_name(a.database_id) as db,
 	d.login_name,
+	convert(decimal(9,2),(a.total_elapsed_time / 1000.) / 60.) as elapsed_minutes,
     substring(b.[text], (statement_start_offset/2) + 1, ((case statement_end_offset when -1 then datalength(b.[text]) else statement_end_offset end - statement_start_offset)/2) + 1) as query_text,
     c.query_plan,
     a.*
